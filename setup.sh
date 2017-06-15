5d318ed58fd071ba1eeaa04e68ed09685988b6b2
Vincent Zhao <gyroknight@users.noreply.github.com>
2017-06-15 14:57:46 -0400

    Added curl, vim, and tmux, simplified commands

--- setup.sh
+++ setup.sh
@@ -4,11 +4,9 @@
 add-apt-repository ppa:fish-shell/release-2 -y
 add-apt-repository ppa:git-core/ppa -y
 apt-get update
-apt-get install git -y
-apt-get install fish -y
+apt-get install git fish vim curl tmux -y
 apt dist-upgrade -fy
 apt-get autoremove -y
 git config --global user.name "Vincent Zhao"
 git config --global user.email "gyroknight@users.noreply.github.com"
 curl -L https://get.oh-my.fish | fish
-fish
