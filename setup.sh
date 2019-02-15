4a0548ce903500220980839b298a6c569869fb80
Vincent Zhao <gyroknight@users.noreply.github.com>
2019-02-15 03:08:18 -0800

    Updated to fish 3 and commented out pip upgrade because it breaks everything

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@
 
 rootpath=$(pwd)
 
-sudo add-apt-repository ppa:fish-shell/release-2 -y
+sudo add-apt-repository ppa:fish-shell/release-3 -y
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:ubuntu-mozilla-daily/ppa -y
 sudo apt-get update
 sudo apt-get install git fish vim curl tmux firefox-trunk htop screenfetch python3-pip build-essential cmake python-dev python3-dev unzip -y
 sudo apt dist-upgrade -fy
 sudo apt-get autoremove -y
-pip3 install --upgrade pip
+# pip3 install --upgrade pip
 git config --global user.name "Vincent Zhao"
 git config --global user.email "gyroknight@users.noreply.github.com"
 cp tmux.conf ~/.tmux.conf
