2b45c3056718a89625b94f427ef0ffa5937d177b
Vincent Zhao <gyroknight@users.noreply.github.com>
2019-12-30 21:09:26 -0500

    Revamped tmux functionality, updated powerline to Python 3.7

--- config.fish
+++ config.fish
@@ -1,15 +1,30 @@
+# Suppress fish greeting
 set fish_greeting
 fish_vi_key_bindings
 
+# If ssh-agent is not active, start ssh-agent
 if not pgrep ssh-agent > /dev/null
-	ssh-agent /usr/bin/fish
+	eval (ssh-agent -c) 
 end
 
-if status is-interactive
-	if tmux ls; and not set -q TMUX
-		tmux a
-	else 
-		tmux new -s main
+# Add to PATH
+set -a PATH ~/.local/bin
+
+# Attach tmux session
+# Creates a session with the given name and attach/switch to it
+function ats
+	if test (count $argv) -eq 1
+		if not tmux has -t $argv > /dev/null 2>&1
+			tmux new -s $argv -d
+		end
+
+		if not set -q TMUX
+			tmux a -t $argv
+		else
+			tmux switch -t $argv
+		end
+	else
+		false
 	end
 end
 
